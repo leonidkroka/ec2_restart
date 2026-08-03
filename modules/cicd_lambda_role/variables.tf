860fccfc5d1c72b21ba46a0bf2a20fe8298885dc
Leonid Kroka <leonid.kroka@gmail.com>
2026-08-03 13:30:24 +0300

    Set the default to Leobit repo

--- modules/cicd_lambda_role/variables.tf
+++ modules/cicd_lambda_role/variables.tf
@@ -1,10 +1,11 @@
 variable "gitlab_project_path" {
-  type        = string
+  type = string
   description = "GitLab address (group/repository or username/repository)"
+  default = "leobit/internal-projects/leobit-sdo/infra-actions"
 }
 
 variable "gitlab_branch" {
-  type        = string
+  type = string
   description = "Allowed branch"
-  default     = "master"
+  default = "master"
 }
